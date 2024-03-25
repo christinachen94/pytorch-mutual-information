1723d10bd8be85acde42508ea23e6524935eb5cd
christinachen94 <christina.chen94@gmail.com>
2024-03-25 14:53:34 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    name = 'pytorch_mi',
+    packages = setuptools.find_packages(),
+    url = 'https://github.com/christinachen94/pytorch-mutual-information'
+)
